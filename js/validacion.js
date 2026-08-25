192eba6bc8e9345efced5e9aab31770602312a4f
dieriverar28 <die.riverar@duocuc.cl>
2026-08-25 11:57:38 -0400

    validacion nombre

--- js/validacion.js
+++ js/validacion.js
@@ -6,7 +6,7 @@ let nombre = document.getElementById("nombre");
 formulario.addEventListener("submit", function(event){
     event.preventDefault();
 
-    if(/^[A-Za-záéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre.value)) {
+    if(!/^[A-Za-záéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre.value)) {
         alert("Nombre solo debe contener letras");
         nombre.focus();
         return;
