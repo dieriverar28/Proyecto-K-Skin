90dcafa68044dc5015111ac3c4c8623d6079032d
dieriverar28 <die.riverar@duocuc.cl>
2026-08-25 11:48:59 -0400

    productos agregados a selccion

---
+++ js/validacion.js
@@ -0,0 +1,15 @@
+
+let formulario = document.getElementById("contacto");
+let nombre = document.getElementById("nombre");
+
+
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    if(/^[A-Za-záéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre.value)) {
+        alert("Nombre solo debe contener letras");
+        nombre.focus();
+        return;
+    }
+    alert("Compra exitosa!");  
+})
